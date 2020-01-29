e79c4f2100517fdb116fc18669dcca37f5ff9e6b
Gavrilajava <georgy.gavrilchik@gmail.com>
2020-01-29 15:31:57 -0600

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.length < 2
+        return array.join("")
+    elsif array.length == 2
+        return array.join(" and ")
+    end
+    last = array.pop
+    array = array.join(", ")
+    array = "#{array}, and #{last}"
+    array
 end
